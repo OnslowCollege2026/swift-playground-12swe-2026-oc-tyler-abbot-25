ab2a5c0c4da45b51ed6520280b3f1b44e495438c
oc-tyler-abbot-25 <tyler.abbot@student.onslow.school.nz>
2026-02-25 21:38:27 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,59 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+func print(board: [[String]]) {
+    board.forEach { line in
+        print("\(line[0]) | \(line[1]) | \(line[2])")
+        print("--+---+--")
+    }
+    print()
+}
+
+func askForPosition (board: [[String]]) -> [Int] {
+    let game = true
+    while game {
+    
+    print("Please enter the row number (1-3): ")
+    let userInput = readLine()!
+    let rowNumber = Int(userInput)! - 1
+
+    print("Please enter the column number (1-3): ")
+    let userInput2 = readLine()!
+    let columnNumber = Int(userInput2)! - 1
+
+    if board[rowNumber][columnNumber] == "." {
+        return [rowNumber, columnNumber]
+    } else {
+        print("Game over.")
+        
+    }
+    }
+}
 
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        var player = "X"
+
+        var board = [
+            [".", ".", "."],
+            [".", ".", "."],
+            [".", ".", "."],
+        ]
+        print(board: board)
+
+        while true {
+        // Ask for the user's move.
+        let position = askForPosition(board: board)
+        board[position[0]][position[1]] = player
+
+        if player == "O" {
+            player = "X"
+        } else {
+            player = "O"
+        }
+
+        print(board: board)
+        
+        }
     }
 }
