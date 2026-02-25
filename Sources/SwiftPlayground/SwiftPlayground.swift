6eb5e0bf0394c5d8d54a796a74cacb9889e9eac3
oc-tyler-abbot-25 <tyler.abbot@student.onslow.school.nz>
2026-02-25 21:36:48 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,15 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let isLarge = false
+    var note = ""
+
+    if isLarge {
+        // note = "Logan is a good person" // Declaring a new value doesn't change the value outside the scope. 
+    } else {
+        note = "Logan is a bad person." // Declaring a new value doesn't change the value outside the scope.
+    }
+
+print(note)
     }
 }
