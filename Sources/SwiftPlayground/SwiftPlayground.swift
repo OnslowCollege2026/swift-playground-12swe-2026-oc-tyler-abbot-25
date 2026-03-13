24c0028d961498a0d837e054800651eaa933f6ff
oc-tyler-abbot-25 <tyler.abbot@student.onslow.school.nz>
2026-03-13 01:53:16 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,22 +1,32 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+/// Parameter:
+/// - board: The 2D grid to display.
 func printBoard(_ board: [[String]]) {
-    
+    for row in board {
+        print(row)
+    }
 }
-
 @main
 struct SwiftPlayground {
     static func main() {
         let size = 6
         var ocean = Array(repeating: Array(repeating: "~", count: size), count: size)
         var guesses = Array(repeating: Array(repeating: "~", count: size), count: size)
+        var rowLabel = " "
 
-
-        while true {
-            print("Where would you like to guess?")
-            print(ocean)
+        for i in 0..<ocean.count {
+            rowLabel += "\(i)"
         }
+        
+        ocean[1][3] = "S"
+        ocean[2][3] = "S"
+        ocean[4][0] = "S"
+        ocean[5][4] = "S"
 
+        print(rowLabel)
+        print(printBoard(ocean))
+        
     }
 }
