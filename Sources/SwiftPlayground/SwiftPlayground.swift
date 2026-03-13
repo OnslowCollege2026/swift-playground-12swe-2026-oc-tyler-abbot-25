2bf5740410f031ba8407e61adffcc1ad496e84be
oc-tyler-abbot-25 <tyler.abbot@student.onslow.school.nz>
2026-03-13 01:27:00 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,22 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+func printBoard(_ board: [[String]]) {
+    
+}
+
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let size = 6
+        var ocean = Array(repeating: Array(repeating: "~", count: size), count: size)
+        var guesses = Array(repeating: Array(repeating: "~", count: size), count: size)
+
+
+        while true {
+            print("Where would you like to guess?")
+            print(ocean)
+        }
+
     }
 }
