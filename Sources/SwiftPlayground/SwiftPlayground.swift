8d07da6f9fd4e3ee8509a3f4235d4013c78f4b99
oc-tyler-abbot-25 <tyler.abbot@student.onslow.school.nz>
2026-03-30 23:54:40 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,56 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+
+        let vocabulary = [
+            ["Hello.",              "Hola", "Y tu", "Bueno", "Decir"],
+            ["Goodbye",  "Adios", "Hola", "Buenos dias", "Necessito"],
+            ["Please",     "Por Favor", "Hola", "Adios", "Lo siento"],
+            ["Thanks", "Gracias", "Lo siento", "Mi opinion", "Hacer"],
+            ["My name is",   "Me llamo", "Hola","Por favor", "Bueno"]
+        ]
+
+        var counter = 0
+        var score = 0
+        var incorrectAnswerIndices: [Int] = []
+
+        while counter < vocabulary.count {
+            let englishWord = vocabulary[counter][0]
+            let correctWord = vocabulary[counter][1]
+            let allAnswers = vocabulary[counter].dropFirst().shuffled()
+
+            print("What is \(englishWord) in Spanish?")
+            allAnswers.forEach { answer in
+                print("- \(answer)")
+            }
+
+            if let userInput = readLine(), userInput.lowercased() == correctWord.lowercased() {
+                score += 1
+                print("Good job, you got it right!")
+            } else {
+                incorrectAnswerIndices.append(counter)
+                print("Incorrect! The right answer is \(correctWord)")
+            }
+
+            counter += 1
+        }
+
+        while incorrectAnswerIndices.count > 0 {
+            let englishWord = vocabulary[counter][0]
+            let correctWord = vocabulary[counter][1]
+            let allAnswers = vocabulary[counter].dropFirst().shuffled()
+
+            print("What is \(englishWord) in Spanish?")
+            allAnswers.forEach { answer in
+                print("- \(answer)")
+            }
+
+            if let userInput = readLine(), userInput.lowercased() == correctWord.lowercased() {
+                incorrectAnswerIndices.removeFirst()
+                print("Good job, you got it right!")
+            } else {
+                print("Incorrect! The right answer is \(correctWord)")
+            }
+        }
     }
 }
