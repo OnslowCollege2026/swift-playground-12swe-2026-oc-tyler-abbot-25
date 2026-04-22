24454f896ea4b34daf5fc6856a0d2c17b65fc2ad
oc-tyler-abbot-25 <tyler.abbot@student.onslow.school.nz>
2026-04-22 23:11:32 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -9,8 +9,8 @@ func stallMenuChoice() -> Int {
         === Roadside Kumara Stall ===
         What would you like to do?
 
-        1. Add kumura to the stock
-        2. Buy a bag of kumura
+        1. Add kumara to the stock
+        2. Buy a bag of kumara
         3. Look at how much stock is left
         4. Find out how many are sold
         5. Leave
@@ -24,18 +24,18 @@ func stallMenuChoice() -> Int {
     }
 }
 
-func addKumuraKg(maximumWeight: Double, kumuraAdded: Double, kumuraStock: Double) -> Double {
-    if kumuraAdded < 0.1 {
+func addKumaraKg(maximumWeight: Double, kumaraAdded: Double, kumaraStock: Double) -> Double {
+    if kumaraAdded < 0.1 {
         print("Invalid amount, you must add at least 100g or 0.1kg of kumura.")
         return 0
     }
 
-    if kumuraAdded + kumuraStock > maximumWeight {
+    if kumaraAdded + kumaraStock > maximumWeight {
         print("Invalid amount, you will exceed the maximum weight of 50 kilograms.")
         return 0
     }
 
-    let newKumuraStock = kumuraAdded + kumuraStock
+    let newKumuraStock = kumaraAdded + kumaraStock
     return newKumuraStock
 }
 
@@ -46,14 +46,14 @@ struct SwiftPlayground {
         /// Used for calculating costs.
         let bagCost:Double = 0.2
         let costPerKg:Double = 3.0
-        let kumuraWeight:Double = 0.1
+        let kumaraWeight:Double = 0.1
 
         /// Lower and upper boundaries 
         let maxStockWeight:Double = 50.0
 
         /// Keeps track of stock and sold counts to update inside functions.
         var stockInKg:Double = 0.0
-        var kumuraWeightSold:Double = 0.0
+        var kumaraWeightSold:Double = 0.0
         /// Used to keep the loop running until the user decides to exit.
         var running:Bool = true
 
@@ -62,13 +62,16 @@ struct SwiftPlayground {
 
             switch choice {
                 case 1:
-                    print("How many kilograms of kumura would you like to add?")
+                    print("How many kilograms of kumara would you like to add?")
+                    if let userInput = readLine(), let amount = Double(userInput) {
+                        stockInKg = addKumaraKg(maximumWeight: maxStockWeight, kumaraAdded: amount, kumaraStock: stockInKg)
+                    }
 
                 case 2:
-                    print("How many kilograms of kumura would you like to buy?")
+                    print("How many kilograms of kumara would you like to buy?")
 
                 case 3:
-                    print("Test")
+                    print("You have \(stockInKg)kg of kumara left.")
 
                 case 4:
                     print("Test")
