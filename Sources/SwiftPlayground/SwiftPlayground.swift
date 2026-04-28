eb29621276526fe0a44d2d5108c184cf08f72953
oc-tyler-abbot-25 <tyler.abbot@student.onslow.school.nz>
2026-04-28 01:14:10 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,6 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+// START COMMENTING, FIX THE PROBLEM WHERE IT STILL SELLS BAGS DESPITE NOT WANTING KUMARA.
+
 func stallMenuChoice() -> Int {
 
     while true {
@@ -27,74 +29,77 @@ func stallMenuChoice() -> Int {
 func addKumaraKg(maximumWeight: Double, kumaraAdded: Double, kumaraStock: Double) -> Double {
     let kumaraWeight: Double = 0.1
 
-    if kumaraAdded < kumaraWeight {
-        print("Invalid amount, you must add at least 0.1kg of kumura.")
+    if kumaraAdded < kumaraWeight && kumaraAdded + kumaraStock > maximumWeight {
+        print("Invalid amount, you can only add between 0.1 and 50kgs of kumara.")
         return 0
+    } else { 
+        let newKumuraStock = kumaraAdded + kumaraStock
+        print("Added \(kumaraAdded)kgs of kumara to the stock.")
+        return newKumuraStock
     }
-
-    if kumaraAdded + kumaraStock > maximumWeight {
-        print("Invalid amount, you will exceed the maximum weight of 50 kilograms.")
-        return 0
-    }
-
-    let newKumuraStock = kumaraAdded + kumaraStock
-    print("Added \(kumaraAdded)kgs of kumara to the stock.")
-    return newKumuraStock
 }
 
 func buyKumara(kumaraStock: Double, amount:Double) -> Double {
-
-    if amount < 0.1 {
-        print("Invalid amount, you have to buy at least 0.1kg of kumara.")
+    if amount < 0.1 && amount > kumaraStock {
+        print("Invalid amount, you can only buy from 0.1 to \(kumaraStock)kgs of kumara.")
         return 0
+    } else { 
+        let newKumuraStock = kumaraStock - amount
+        print("Sold \(amount)kgs of kumara. There is now \(newKumuraStock)kgs of kumara left.")
+        return newKumuraStock
     }
-
-    if amount > kumaraStock {
-        print("Invalid amount, we don't have that much kumara.")
-        return 0
-    }
-
-    let newKumuraStock = kumaraStock - amount
-    print("Sold \(amount)kgs of kumara. There is now \(newKumuraStock)kgs of kumara left.")
-    return newKumuraStock
 }
 
-// FIX THIS
-func costPerSale(kumaraStock:Double, amount:Double) -> Double {
-    let bagCosts: Double = 0.2
-    let costPerKg: Double = 3.0
-    let maxBagWeight:Double = 5.0
+func costs(kumaraStock: Double, amount:Double, bagsUsed:Double) -> Double {
+    let bagPrice: Double = 0.2
+    let pricePerKg: Double = 3.0
 
-    var bagCount:Double = 1.0
-
-    if amount < 0.1 {
+    if amount < 0.1 && amount > kumaraStock, bagsUsed < 1 {
         return 0
+    } else {
+        let costs = (pricePerKg * amount) + (bagPrice * bagsUsed)
+        print("Total Cost: $\(costs).")
+        return costs
     }
+}
 
-    if amount > kumaraStock {
-        return 0
+func bagsUsed(amount: Double, kumaraStock: Double, numberOfBags: Double) -> Double {
+    let maximumBags:Double = 5000
+    let maximumWeight:Double = 5.0
+    var amountLeft:Double = amount
+    var bagsUsed:Double = 0.0
+
+
+    if amount < 0.1 && amount > kumaraStock {
+        return bagsUsed
+    } 
+
+    if numberOfBags > maximumBags {
+        return bagsUsed
+    } 
+
+    bagsUsed += 1
+    while amountLeft > maximumWeight {
+        amountLeft = amount - maximumWeight 
+        bagsUsed += 1
     }
-
-    while amount > maxBagWeight {
-        bagCount += 1
-    }
-
-    let totalCosts = (amount * costPerKg) + (bagCosts * bagCount)
-    print("Total Price: $\(totalCosts).")
-    return totalCosts
+    return bagsUsed
+    
 }
 
 @main
 struct SwiftPlayground {
     static func main() {
 
-        /// Lower and upper boundaries
+        /// Boundaries and prices for calculations.
         let maxStockWeight: Double = 50.0
 
         /// Keeps track of stock and sold counts to update inside functions.
         var stockInKg: Double = 0.0
-        var salesRecords: [Double] = []
-        var kumaraWeightSold: Double = 0.0
+        var totalUsedBags: Double = 0.0
+        var numberOfBags: [Double] = []
+        var kumaraWeightSold: [Double] = []
+        var costsRecord: [Double] = []
         /// Used to keep the loop running until the user decides to exit.
         var running: Bool = true
 
@@ -114,17 +119,20 @@ struct SwiftPlayground {
             case 2:
                 print("How many kilograms of kumara would you like to buy?")
                 if let userInput = readLine(), let amount = Double(userInput) {
-                    kumaraWeightSold += buyKumara(kumaraStock: stockInKg, amount: amount)
-                    // FIX THIS 
-                    salesRecords.append(costPerSale(kumaraStock: stockInKg, amount: amount))
+                    // Appends so that it can be compared in a sales record later.
+                    kumaraWeightSold.append(buyKumara(kumaraStock: stockInKg, amount: amount))
+                    numberOfBags.append(bagsUsed(amount: amount, kumaraStock: stockInKg, numberOfBags: totalUsedBags))
+                    totalUsedBags += bagsUsed(amount: amount, kumaraStock: stockInKg, numberOfBags: totalUsedBags)
+
+                    // Adds bags used so that it can calculate the total cost and add it to the records.
+                    costsRecord.append(costs(kumaraStock: stockInKg, amount: amount, bagsUsed: bagsUsed(amount: amount, kumaraStock: stockInKg, numberOfBags: totalUsedBags)))
                 }
 
             case 3:
                 print("You have \(stockInKg)kg of kumara left.")
 
             case 4:
-                // START ON THIS
-                print("Test")
+                let salesRecord = Array(zip(kumaraWeightSold, costsRecord))
 
             case 5:
                 print("Exiting programme, have a good day.")
